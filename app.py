c2a8185dd120c04d660a152df5d9beeced07ccee
Satyam Tiwary <satyam@technoculture.io>
2022-12-24 16:16:16 +0530

    feat: add geojson converter

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from dataclasses import dataclass
-from typing import Optional, Self
+from typing import Optional, Self, Dict, Tuple, NamedTuple
 import ezdxf as ez
 from io import BytesIO, StringIO
+import re
+import math
+import geojson
 
 
 @dataclass
@@ -16,17 +19,12 @@ class LongLat:
         regex to convert a string in the format "N 4792384, E 798797" to a LongLat object
         '''
         print(string)
-
-        import re
         pattern = r'([NS])\s*(\d+\.\d+),\s*([EW])\s*(\d+\.\d+)'
-
         match = re.search(pattern, string)
-
-        print(match)
+        # print(match)
 
         if match is None:
             return None
-
         return cls(longitude=float(match.group(2)), latitude=float(match.group(4)))
 
 
@@ -36,13 +34,128 @@ class CalPoint:
     longlat: LongLat
 
 
+def file_found(entity):
+    st.success(
+        f"Found in file: Entity={entity}, Layer={entity.dxf.layer}")
+
+
+@dataclass
+class DxfGeometry:
+    point_a: CalPoint
+    point_b: CalPoint
+    point_c: CalPoint
+    boundary: ez.entities.Polyline
+
+
+def process_dxf_file(dxf_file: ez.document.Drawing):
+    dxf_cal_points = {}
+    dxf_boundary = None
+
+    for entity in dxf_file.modelspace():
+        match entity.dxftype():
+            case 'POLYLINE':
+                if entity.dxf.layer == "POLYLINE":
+                    dxf_boundary = entity
+                    file_found(entity)
+            case 'POINT':
+                if entity.dxf.layer in ["POINT_A", "POINT_B", "POINT_C"]:
+                    dxf_cal_points[entity.dxf.layer] = CalPoint(
+                        name=entity.dxf.layer, longlat=LongLat(longitude=entity.dxf.location[0], latitude=entity.dxf.location[1]))
+                    file_found(entity)
+
+    # st.write(dxf_cal_points)
+    return DxfGeometry(
+        dxf_cal_points["POINT_A"], dxf_cal_points["POINT_B"], dxf_cal_points["POINT_C"], dxf_boundary)
+
+
+class Transform(NamedTuple):
+    s: float
+    dx: float
+    dy: float
+    theta: float
+
+
+def calculate_transform_params(cal_points: Dict[str, CalPoint], dxf_geometry: DxfGeometry) -> Transform:
+    dx = cal_points["A"].longitude - dxf_geometry.point_a.longlat.longitude
+    dy = cal_points["A"].latitude - dxf_geometry.point_a.longlat.latitude
+
+    dxa = cal_points["B"].longitude - cal_points["A"].longitude
+    dya = cal_points["B"].latitude - cal_points["A"].latitude
+    dxb = dxf_geometry.point_b.longlat.longitude - \
+        dxf_geometry.point_a.longlat.longitude
+    dyb = dxf_geometry.point_b.longlat.latitude - \
+        dxf_geometry.point_a.longlat.latitude
+
+    s = (dxa * dxb + dya * dyb) / (dxb**2 + dyb**2)
+    theta = math.atan2(dya, dxa) - math.atan2(dyb, dxb)
+
+    return Transform(s, dx, dy, theta)
+
+
+def generate_calibrated_geojson(cal_points: Dict[str, LongLat], dxf_geometry: DxfGeometry) -> geojson.Feature:
+    s, dx, dy, theta = calculate_transform_params(cal_points, dxf_geometry)
+
+    def transform_point(point_dxf: CalPoint) -> Tuple[float, float]:
+        x_dxf = point_dxf.longlat.longitude
+        y_dxf = point_dxf.longlat.latitude
+        x_real = s * x_dxf + dx
+        y_real = s * y_dxf + dy
+        print(
+            f"Transformed point {point_dxf.name} from {x_dxf}, {y_dxf}: {x_real}, {y_real}")
+        return x_real, y_real
+
+    # transform calibration points
+    point_a_real = transform_point(dxf_geometry.point_a)
+    point_b_real = transform_point(dxf_geometry.point_b)
+    point_c_real = transform_point(dxf_geometry.point_c)
+
+    # transform boundary polygon
+    boundary_vertices_real = []
+    for vertex in dxf_geometry.boundary.vertices:
+        print(vertex)
+        x_dxf = vertex.dxf.location[0]
+        y_dxf = vertex.dxf.location[1]
+        x_real = s * x_dxf + dx
+        y_real = s * y_dxf + dy
+        boundary_vertices_real.append((x_real, y_real))
+
+    # construct GeoJSON object
+    geometry = geojson.Polygon([boundary_vertices_real])
+    properties = {
+        "point_a": {
+            "name": dxf_geometry.point_a.name,
+            "longitude": point_a_real[0],
+            "latitude": point_a_real[1]
+        },
+        "point_b": {
+            "name": dxf_geometry.point_b.name,
+            "longitude": point_b_real[0],
+            "latitude": point_b_real[1]
+        },
+        "point_c": {
+            "name": dxf_geometry.point_c.name,
+            "longitude": point_c_real[0],
+            "latitude": point_c_real[1]
+        }
+    }
+
+    feature = geojson.Feature(geometry=geometry, properties=properties)
+    return feature
+
+
+def generate_geojson(dxf_geometry, trans):
+    boundary = dxf_geometry.boundary
+    geojson_boundary = geojson.Polygon()
+
+
 if __name__ == "__main__":
     st.image('https://www.vastuvihar.org/images/vastulogo.png', width=180)
     st.title("AutoCal")
     st.write("Convert Dwg Files to geoJSON")
 
-    file = st.file_uploader("Upload Dwg File", type=["dxf"])
+    dxf_geometry: Optional[DxfGeometry] = None
 
+    file = st.file_uploader("Upload Dxf File", type=["dxf"])
     dxf_file = None
     if file is not None:
         file_val = StringIO(file.getvalue().decode('utf-8'))
@@ -51,12 +164,9 @@ if __name__ == "__main__":
         except ez.DXFStructureError as e:
             st.error("DXF file is not valid. {}".format(e))
 
-        # print(dwg)
+        dxf_geometry = process_dxf_file(dxf_file)
 
     if dxf_file is not None:
-        # cal_point_count = st.number_input("Number of Calliberation Points", min_value=3, max_value=10, value=3)
-        cal_point_count = 3
-
         cal_points_str = {}
         cal_points_str["A"] = st.text_input(f"Cal Point A")
         cal_points_str["B"] = st.text_input(f"Cal Point B")
@@ -75,5 +185,11 @@ if __name__ == "__main__":
                 break
 
         if invalid_count == 0:
-            if st.button("Convert"):
-                st.write(cal_points)
+            if st.button("Generate GeoJSON"):
+                print(cal_points)
+                print(dxf_geometry)
+
+                if dxf_geometry is not None:
+                    calibrated_geojson = generate_calibrated_geojson(
+                        cal_points, dxf_geometry)
+                    st.write(calibrated_geojson)
